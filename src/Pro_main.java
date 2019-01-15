ee69acc3ccb4fc406f24d5ff67cbe28797de0550
이재윤 <kjd99002@naver.com>
2019-01-16 01:52:02 +0900

    Start
    원본

---
+++ src/Pro_main.java
@@ -0,0 +1,14 @@
+
+public class Pro_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int admin = 0;
+		int JY=0;
+		int j=0;
+
+	}
+
+}
---
+++ src/clde.java
@@ -0,0 +1,9 @@
+
+public class clde {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
